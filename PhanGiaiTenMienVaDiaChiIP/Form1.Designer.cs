f2f5f02398b8d5d24c4f9902c0665ca4155f4020
USER <USER@ASUS>
2018-03-06 14:53:55 +0700

    Add project files.

---
+++ PhanGiaiTenMienVaDiaChiIP/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PhanGiaiTenMienVaDiaChiIP
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHostName = new System.Windows.Forms.TextBox();
+            this.listBoxIPAddress = new System.Windows.Forms.ListBox();
+            this.listBoxAlias = new System.Windows.Forms.ListBox();
+            this.btnResolve = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(146, 43);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(185, 22);
+            this.txtIP.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Host name:";
+            // 
+            // txtHostName
+            // 
+            this.txtHostName.Location = new System.Drawing.Point(146, 92);
+            this.txtHostName.Name = "txtHostName";
+            this.txtHostName.Size = new System.Drawing.Size(185, 22);
+            this.txtHostName.TabIndex = 3;
+            // 
+            // listBoxIPAddress
+            // 
+            this.listBoxIPAddress.FormattingEnabled = true;
+            this.listBoxIPAddress.ItemHeight = 16;
+            this.listBoxIPAddress.Location = new System.Drawing.Point(63, 208);
+            this.listBoxIPAddress.Name = "listBoxIPAddress";
+            this.listBoxIPAddress.Size = new System.Drawing.Size(231, 180);
+            this.listBoxIPAddress.TabIndex = 4;
+            // 
+            // listBoxAlias
+            // 
+            this.listBoxAlias.FormattingEnabled = true;
+            this.listBoxAlias.ItemHeight = 16;
+            this.listBoxAlias.Location = new System.Drawing.Point(341, 208);
+            this.listBoxAlias.Name = "listBoxAlias";
+            this.listBoxAlias.Size = new System.Drawing.Size(231, 180);
+            this.listBoxAlias.TabIndex = 5;
+            // 
+            // btnResolve
+            // 
+            this.btnResolve.Location = new System.Drawing.Point(419, 43);
+            this.btnResolve.Name = "btnResolve";
+            this.btnResolve.Size = new System.Drawing.Size(75, 23);
+            this.btnResolve.TabIndex = 6;
+            this.btnResolve.Text = "Resolve";
+            this.btnResolve.UseVisualStyleBackColor = true;
+            this.btnResolve.Click += new System.EventHandler(this.btnResolve_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "IP Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(352, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Alias";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 456);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnResolve);
+            this.Controls.Add(this.listBoxAlias);
+            this.Controls.Add(this.listBoxIPAddress);
+            this.Controls.Add(this.txtHostName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtIP;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtHostName;
+		private System.Windows.Forms.ListBox listBoxIPAddress;
+		private System.Windows.Forms.ListBox listBoxAlias;
+		private System.Windows.Forms.Button btnResolve;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+	}
+}
+
---
+++ PhanGiaiTenMienVaDiaChiIP/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+namespace PhanGiaiTenMienVaDiaChiIP
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnResolve_Click(object sender, EventArgs e)
+		{
+			string ip = txtIP.Text;
+			IPHostEntry iphe = Dns.Resolve(ip); //Phan giai IP
+			txtHostName.Text = iphe.HostName;
+			foreach (string s in iphe.Aliases)
+				listBoxIPAddress.Items.Add(s);
+			foreach (IPAddress a in iphe.AddressList)
+				listBoxAlias.Items.Add(a.ToString());
+		}
+	}
+}
